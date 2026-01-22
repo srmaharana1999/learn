3c93715ced04c3a13659236134502194d40b28ae
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2026-01-22 14:05:18 +0530

    deploy for analytics testing

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
-
+const name = "srm";
 export const metadata: Metadata = {
-  title: "Login ",
+  title: `${name}`,
 };
 export default function Login() {
   return <h1>Login Route</h1>;
